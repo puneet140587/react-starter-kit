11d9b41ec1043f45fc147ad9b5d2b5957d9fad05
Puneet Shrivastava <Puneet.Shrivastava@ymail.com>
2024-05-26 08:08:16 +0530

    Testing git Macbook Connectivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>Hello world!</div>;
+  return <div>Hello world! This is the testing being carried over Macbook air
+
+<h1 style={{backgroundColor : "red"}}>This is agai being done for testing purpose</h1>
+
+  </div>;
 };
 
-export default App;
+export default App;  
