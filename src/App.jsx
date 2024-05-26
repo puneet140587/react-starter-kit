edbc35f3b83d82fcdd5680bfeb9d7d1f2aba34a8
Puneet Shrivastava <Puneet.Shrivastava@ymail.com>
2024-05-26 08:08:46 +0530

    Testing COnnectivity with Git

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 const App = () => {
   return <div>Hello world! This is the testing being carried over Macbook air
 
-<h1 style={{backgroundColor : "red"}}>This is agai being done for testing purpose</h1>
+    <h1 style={{ backgroundColor: "red" }}>This is again being done for testing purpose</h1>
+    
 
   </div>;
 };
